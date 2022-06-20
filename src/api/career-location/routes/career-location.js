7eb6b5db787dec1c10183832ae4c20af8d72383a
yacob <yacob.huang@gmail.com>
2022-06-20 14:31:17 +0700

    career module

---
+++ src/api/career-location/routes/career-location.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * career-location router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::career-location.career-location');
---
+++ src/api/career-location/services/career-location.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * career-location service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::career-location.career-location');
