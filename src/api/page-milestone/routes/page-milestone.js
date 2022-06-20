450a242977ed189e166cea6fb6e94ddfa0a265f2
yacob <yacob.huang@gmail.com>
2022-06-20 22:01:10 +0700

    milestone module

---
+++ src/api/page-milestone/routes/page-milestone.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * page-milestone router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::page-milestone.page-milestone');
---
+++ src/api/page-milestone/services/page-milestone.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * page-milestone service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::page-milestone.page-milestone');
