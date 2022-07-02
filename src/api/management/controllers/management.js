d0eec6d84c2234a47eea0fdeeedc1eebed219966
yacob <yacob.huang@gmail.com>
2022-07-02 15:53:01 +0700

    rename mgmt and org

--- src/api/management/controllers/management.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  management controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::management.management');
--- src/api/page-management/routes/page-management.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * page-management router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::page-management.page-management');
