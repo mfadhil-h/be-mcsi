19090d022ca11275e733aaca31350b9d9ae0cdd1
Zhaky Hadi <zhaky.gng@gmail.com>
2022-07-04 19:25:34 +0800

    Redirect strapi 404 error to Nuxt

--- src/middlewares/nuxtSPA.js
+++ src/middlewares/nuxtSPA.js
@@ -8,17 +8,35 @@ const path = require('path');
 module.exports = (config, { strapi }) => {
     const adminPath = strapi.config.admin.path.replace("/", '')
     const apiPath = strapi.config.api.rest.prefix.replace("/", '')
-    const ignoredPath = [apiPath, adminPath, "uploads", "_nuxt"]
+    const restrictedPath = [apiPath, adminPath, "uploads", "_nuxt"]
 
     const staticDir = path.resolve(strapi.config.server.dirs.public)
     return async (ctx, next) => {
         let paths = ctx.request.url.split('/')
-        if (paths.length > 2 && ctx.request.method == 'GET' && !ignoredPath.includes(paths[1])) {
-            paths[paths.length - 1] = 'index.html'
-
-            ctx.type = 'html';
-            ctx.body = fs.createReadStream(path.join(staticDir + paths.join("/")));
+        if (paths.length > 2 && ctx.request.method == 'GET' && !restrictedPath.includes(paths[1])) {
+            serveRelativeIndex(ctx, staticDir, paths)
         }
         await next();
+
+        // fallback to root index html if strapi return 404 on non restricted path
+        if (ctx.request.method == 'GET' && !restrictedPath.includes(paths[1]) && ctx.response.status == 404) {
+            ctx.type = 'html';
+            ctx.body = fs.createReadStream(path.join(staticDir + "/index.html"));
+        }
     };
 };
+
+
+// Serve index.html on each respective folder
+function serveRelativeIndex(ctx, staticDir, paths) {
+    paths[paths.length - 1] = 'index.html'
+    let indexPage = path.join(staticDir + paths.join("/"));
+
+    if (fs.existsSync(indexPage)) {
+        ctx.type = 'html';
+        ctx.body = fs.createReadStream(indexPage);
+    } else if (paths.length > 2) {
+        paths.pop()
+        serveRelativeIndex(ctx, staticDir, paths)
+    }
+}
