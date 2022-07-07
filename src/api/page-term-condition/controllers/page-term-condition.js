66b456c80612b70ac3df75ecd7c83e53d5964eb7
yacob <yacob.huang@gmail.com>
2022-07-07 14:27:40 +0700

    add static page: PP, TC, sitemap

---
+++ src/api/page-term-condition/controllers/page-term-condition.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  page-term-condition controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::page-term-condition.page-term-condition');
