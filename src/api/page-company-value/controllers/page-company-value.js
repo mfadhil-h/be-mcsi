0d40f989494870a4d39f072bc1b2f148f7701d1f
yacob <yacob.huang@gmail.com>
2022-09-06 10:40:02 +0700

    add company value
    Co-authored-by: zhakyhadi <zhakyhadi@users.noreply.github.com>

---
+++ src/api/page-company-value/controllers/page-company-value.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  page-company-value controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::page-company-value.page-company-value');
---
+++ src/api/page-company-value/routes/page-company-value.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * page-company-value router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::page-company-value.page-company-value');
