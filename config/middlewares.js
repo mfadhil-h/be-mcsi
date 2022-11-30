11f567c4e862b94f73423f5b718c238d0a0846f3
Zhaky Hadi <zhaky.gng@gmail.com>
2022-11-30 13:06:52 +0800

    Add CSP directive for script

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,25 +1,34 @@
 module.exports = [
-  'strapi::errors',
+  "strapi::errors",
   {
-    name: 'strapi::security',
+    name: "strapi::security",
     config: {
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          'frame-src': ['youtube.com', 'www.youtube.com'],
+          "frame-src": ["youtube.com", "www.youtube.com"],
+          "script-src": [
+            "'self'",
+            "'script-src'",
+            "'unsafe-inline'",
+            "'script-src-elem",
+            "www.google-analytics.com",
+            "www.googletagmanager.com",
+            "google-analytics.com",
+          ],
           upgradeInsecureRequests: null,
         },
       },
     },
   },
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 
-  'global::nuxtSPA'
+  "global::nuxtSPA",
 ];
