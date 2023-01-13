1b04a4a92e50b22e8796c70d66e14e74af2abbe4
Zhaky Hadi <zhaky.gng@gmail.com>
2023-01-13 13:03:24 +0800

    update CSP for GTM

--- config/middlewares.js
+++ config/middlewares.js
@@ -6,15 +6,31 @@ module.exports = [
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          "frame-src": ["youtube.com", "www.youtube.com"],
+          "frame-src": [
+            "youtube.com",
+            "www.youtube.com",
+            "https://bid.g.doubleclick.net",
+          ],
           "script-src": [
             "'self'",
             "'unsafe-inline'",
-            "www.google-analytics.com",
-            "www.googletagmanager.com",
-            "google-analytics.com",
-            "www.googleadservices.com"
+            "https://www.google-analytics.com",
+            "https://ssl.google-analytics.com",
+            "https://*.googletagmanager.com",
+            "https://www.googleadservices.com",
+            "https://googleads.g.doubleclick.net",
+            "https://*.google.com",
           ],
+          "connect-src": [
+            "https://*.google-analytics.com",
+            "https://*.analytics.google.com",
+            "https://*.googletagmanager.com",
+          ],
+          // "img-src": [
+          //   "https://*.google-analytics.com",
+          //   "https://*.googletagmanager.com",
+          //   "https://www.google.com",
+          // ],
           upgradeInsecureRequests: null,
         },
       },
